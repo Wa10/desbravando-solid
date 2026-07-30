9c73b07bdf14735a166259c2739ddffd55f8e953
Walace Alves <walace.alves10@gmail.com>
2026-07-30 15:24:47 -0300

    refactor: introduce ebook domain model and simplify generators

---
+++ src/main/java/cotuba/GeradorDeTituloDoCapitulo.java
@@ -0,0 +1,30 @@
+package cotuba;
+
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.Heading;
+import org.commonmark.node.Node;
+import org.commonmark.node.Text;
+
+public class GeradorDeTituloDoCapitulo extends AbstractVisitor {
+
+    private String titulo;
+
+    public String extraiTitulo(Node document) {
+        document.accept(this);
+        return this.titulo;
+    }
+
+    @Override
+    public void visit(Heading heading) {
+        // Pega apenas o primeiro título H1 encotrado
+        if (heading.getLevel() == 1 && this.titulo == null) {
+            if (heading.getFirstChild() instanceof Text text) {
+                this.titulo = text.getLiteral();
+            }
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
